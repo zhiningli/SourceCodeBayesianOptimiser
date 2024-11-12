d1231b6b744205d20814e88f83b3ffafe151ce2a
Zhining Li <zhiningli.dev@gmail.com>
2024-11-12 17:57:10 +0000

    implemented all components of SVM BO optimiser

--- dev.py
+++ dev.py
@@ -12,27 +12,3 @@ source_code_status = SourceCodeStatus.FAILED_VALIDATION.value
 
 source_codes_pending_validation = sourceCodeRepository.find_source_codes(status=source_code_status, limit = 30)
 
-source_code_validator = SourceCodeValidator()
-fail_to_update_source_code = []
-# Step2: processing source code from the list one by each time
-for source_code_object in source_codes_pending_validation:
-
-    source_code_id = source_code_object["_id"]
-    source_code_block = source_code_object['source_code'] 
-
-    validated_source_code = source_code_validator.iterative_refinement_and_validation(source_code_block)
-    if validated_source_code:
-        source_code_object['source_code'] = validated_source_code
-        source_code_object['status'] = SourceCodeStatus.VALIDATED_TO_RUN.value
-        source_code_object['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        sourceCodeRepository.update_source_code(record_id=source_code_id, update_data= source_code_object)
-    else:
-        logging.warning(f"source code validation for source code {source_code_id} has failed")
-        fail_to_update_source_code.append(source_code_object)
-        source_code_object['source_code'] = source_code_block
-        source_code_object['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        source_code_object['status'] = SourceCodeStatus.FAILED_VALIDATION.value
-        sourceCodeRepository.update_source_code(record_id=source_code_id, update_data= source_code_object)
-
-
-print(fail_to_update_source_code)
--- src/preprocessing/SVM_BO_optimiser.py
+++ src/preprocessing/SVM_BO_optimiser.py
@@ -3,10 +3,123 @@ from gpytorch.priors import NormalPrior
 from gpytorch.models import ExactGP
 from gpytorch.means import ConstantMean
 from gpytorch.distributions import MultitaskMultivariateNormal
-from botorch.acquisition import ExpectedImprovement
-import torch
 
-class SVM_Expected_Improvement:
+from botorch.acquisition import LogExpectedImprovement
+from botorch.optim import optimize_acqf
+from botorch.utils.transforms import standardize
+from botorch.utils.sampling import draw_sobol_samples
+from botorch.optim.fit import fit_gpytorch_mll_torch
+from gpytorch.mlls import ExactMarginalLogLikelihood
+
+from src.data.data_models import SVMHyperParameterSpace
+from src.training.bo_metrics import cumulative_regret
+
+import numpy as np
+import torch
+import traceback
+import logging
+
+class SVM_BO_optimiser:
+
+    def __init__(self):
+        self.objective_func_hyperparameter_search_space = SVMHyperParameterSpace
+        self.params = None
+        self.objective_func = None
+    
+    def optimise(self, code_str, n_iter=20, initial_points=10, sample_per_batch=1):
+        try:
+            namespace = {}
+            exec(code_str, namespace)
+            # Retrieve the function dynamically
+            self.objective_func = namespace.get("run_svm_classification")
+            if not callable(self.objective_func):
+                raise ValueError("The code string must define a callable 'run_svm_classification' function.")
+            
+            # Run Bayesian optimization
+            return self._run_bayesian_optimisation(n_iter=n_iter, initial_points=initial_points, sample_per_batch=sample_per_batch)
+        except Exception as e:
+            error_message = traceback.format_exc()
+            logging.error("Execution failed with error: %s", error_message)
+            self.last_error = error_message
+            return False
+        
+    def _botorch_objective(self, x):
+        np_params = x.detach().numpy().flatten()
+        kernel_idx = int(np.round(np_params[0]))
+        gamma_idx = int(np.round(np_params[3]))
+
+        kernel = SVMHyperParameterSpace["kernel"]["options"][kernel_idx]
+        C = np_params[1]
+        coef0 = np_params[2]
+        gamma = SVMHyperParameterSpace["gamma"]["options"][gamma_idx]
+
+        # Call the dynamically assigned function with parameters
+        result = self.objective_func(kernel=kernel, C=C, coef0=coef0, gamma=gamma)
+        return torch.tensor(result, dtype=torch.float64)  # Ensure the result tensor is also double precision
+
+    def _run_bayesian_optimisation(self, 
+                                   n_iter, 
+                                   initial_points, 
+                                   sample_per_batch,
+                                   svm_kernel_outputscale_mean):
+        # Define the bounds for kernel, C, coef0, and gamma search space
+        bounds = torch.tensor([[0, 0.1, 0.0, 0], [len(SVMHyperParameterSpace["kernel"]["options"])-1, 10.0, 1.0, len(SVMHyperParameterSpace["gamma"]["options"])-1]], dtype=torch.float64)
+
+        # Step 1: Initial Sample Points for Training
+        train_x = draw_sobol_samples(bounds=bounds, n=initial_points, q=sample_per_batch).squeeze(1).to(dtype=torch.float64)
+        train_x = (train_x - bounds[0]) / (bounds[1] - bounds[0])
+        unnormalized_train_x = train_x * (bounds[1] - bounds[0]) + bounds[0]
+
+        train_y = torch.tensor([self._botorch_objective(x) for x in unnormalized_train_x], dtype=torch.float64).unsqueeze(-1)
+        train_y = standardize(train_y).view(-1, 1)
+
+        kernel = (
+            SVM_BO_Kernel.builder()
+            .build_kernel_for_svm_kernel(outputscale_prior_mean=svm_kernel_outputscale_mean)
+            .build_kernel_for_svm_C(lengthscale_prior_mean=1.0, outputscale_prior_mean=1.5)
+            .build_kernel_for_svm_gamma(outputscale_prior_mean=1.2)
+            .build_kernel_for_svm_coef0(lengthscale_prior_mean=0.5, outputscale_prior_mean=1.0)
+            .build()
+        )
+
+        # Initialise the GP model with double precision
+        mll = ExactMarginalLogLikelihood().to(torch.float64)
+
+        gp = SVM_GP_model(train_X=train_x, train_y=train_y, likelihood=mll, kernel=kernel).to(torch.float64)
+
+        # Fit the GP model
+        fit_gpytorch_mll_torch(mll)
+
+        ei = SVM_Expected_Improvement(model=gp, best_f=train_y.max())
+
+        candidates = []
+        for i in range(n_iter):
+            candidate, _ = optimize_acqf(
+                acq_function=ei,
+                bounds=bounds,
+                q=sample_per_batch,
+                num_restarts=5,
+                raw_samples=20
+            )
+
+            candidates.append(candidate)
+
+            train_y = train_y.view(-1, 1)   
+            # Evaluate the candidate and ensure new_y has shape [1, 1]
+            new_y = self._botorch_objective(candidate).view(1, 1)  # Ensure new_y shape [1, 1]
+            # Concatenate the new candidate and new_y to training data
+            train_x = torch.cat([train_x, candidate.view(1, -1)])  # Ensure train_x matches shape
+            train_y = torch.cat([train_y, new_y], dim=0)  # Concatenate along dimension 0
+            train_y = train_y.view(-1)
+
+            gp.set_train_data(inputs=train_x, targets=train_y, strict=False)
+            ei = SVM_Expected_Improvement(model=gp, best_f=train_y.max())
+
+        return candidates
+
+
+
+class SVM_Expected_Improvement(LogExpectedImprovement):
 
     def __init__(self, model, best_f, constraints):
         super().__init__(model, best_f)
