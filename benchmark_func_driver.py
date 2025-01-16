ca39d119cf49ff98e6bc363fef6b99f13e604dae
Zhining Li <zhiningli.dev@gmail.com>
2025-01-16 13:31:11 +0000

    remove extra code files

--- benchmark_func_driver.py
+++
@@ -1,8 +0,0 @@
-from src.utils.benchmark_functions.benchmark_functions import Rastrigin, Beale, Sphere, BinaryTreeStructuredFunction
-
-
-rastrigin = Rastrigin(n_dimension=2, noises=0.1, irrelevant_dims=0)
-
-print(rastrigin.describe)
-print(rastrigin.get_source_code())
-
